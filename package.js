daa79216a9f413deb9512e4a3fd5225dd3102ab3
apendua <apendua@gmail.com>
2014-01-08 12:06:25 +0100

    Compatibility with older meteor versions

--- package.js
+++ package.js
@@ -7,6 +7,8 @@ Package.on_use(function (api) {
     api.use(['random', 'underscore'], ['client', 'server']);
     api.add_files('require.js', ['client', 'server']);
 
-    api.export('define', ['client', 'server']);
-    api.export('require', ['client', 'server']);
+    if (api.export !== undefined) {
+      api.export('define', ['client', 'server']);
+      api.export('require', ['client', 'server']);
+    }
 });
