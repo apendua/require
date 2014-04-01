24f05a0f9298a2ec10ade86c41284e8bafbb40fd
apendua <apendua@gmail.com>
2014-04-01 16:15:09 +0200

    Added tests

--- package.js
+++ package.js
@@ -19,3 +19,15 @@ Package.on_use(function (api) {
     api.export('require', ['client', 'server']);
   }
 });
+
+
+Package.on_test(function (api) {
+  // ENVIRONMENT
+  api.use('tinytest', ['client', 'server']);
+  api.add_files('manager.js', ['client', 'server']);
+
+  // TESTS
+  api.add_files([
+    'tests.js',
+  ], 'client');
+});
