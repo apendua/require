125680e964d32eabeacf64268a06609b6de5003a
apendua <apendua@gmail.com>
2014-04-01 16:12:30 +0200

    Renaming the main source file

--- package.js
+++ package.js
@@ -5,7 +5,7 @@ Package.describe({
 
 Package.on_use(function (api) {
     api.use(['random', 'underscore'], ['client', 'server']);
-    api.add_files('require.js', ['client', 'server']);
+    api.add_files('manager.js', ['client', 'server']);
 
     if (api.export !== undefined) {
       api.export('define', ['client', 'server']);
